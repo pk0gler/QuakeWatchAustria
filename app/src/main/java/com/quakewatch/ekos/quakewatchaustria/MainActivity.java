39e7ce45c2a5434582e240d1cd8477ff1554c1dc
pkogler <pkogler@student.tgm.ac.at>
2015-10-24 14:38:00 +0200

    changed Settings activity and customized Settings Layout

--- app/src/main/java/com/quakewatch/ekos/quakewatchaustria/MainActivity.java
+++ app/src/main/java/com/quakewatch/ekos/quakewatchaustria/MainActivity.java
@@ -1,10 +1,13 @@
 package com.quakewatch.ekos.quakewatchaustria;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -56,8 +59,11 @@ public class MainActivity extends AppCompatActivity {
         //Setting the Viewpager for the SlidingTabsLayout
         tabs.setViewPager(pager);
 
-
-
+        SharedPreferences SP = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+        String email = SP.getString("email", "");
+        boolean notif = SP.getBoolean("notifications", false);
+        String magn = SP.getString("magType", "1");
+        Log.d("pref","Email Addresse: "+email+"  -  Notifications: "+notif + "  -  Magnitude: "+magn);
     }
 
     @Override
--- app/src/main/java/com/quakewatch/ekos/quakewatchaustria/SubACtivities/SubActivity_SettingsActivity.java
+++ app/src/main/java/com/quakewatch/ekos/quakewatchaustria/SubACtivities/SubActivity_SettingsActivity.java
@@ -1,7 +1,10 @@
 package com.quakewatch.ekos.quakewatchaustria.SubACtivities;
 
 import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.util.Log;
 
 import com.quakewatch.ekos.quakewatchaustria.R;
 
@@ -12,6 +15,27 @@ public class SubActivity_SettingsActivity extends PreferenceActivity {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.preferences);
+		//super.findPreference("email").setOnPreferenceChangeListener(new PrefListener(SubActivity_SettingsActivity.this));
+		//super.findPreference("notifications").setOnPreferenceChangeListener(new PrefListener(SubActivity_SettingsActivity.this));
+		findPreference("email").setOnPreferenceChangeListener(
+				new Preference.OnPreferenceChangeListener() {
+
+					@Override
+					public boolean onPreferenceChange(Preference preference, Object newValue) {
+						EditTextPreference prefText = (EditTextPreference) preference;
+						Log.d("isRichtigDiemal", prefText.getText()+"----"+isValidEmail((String)newValue));
+						return isValidEmail((String)newValue);
+					}
+
+				});
+	}
+
+	public final static boolean isValidEmail(CharSequence target) {
+		if (target == null) {
+			return false;
+		} else {
+			return android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
+		}
 	}
 
 }
