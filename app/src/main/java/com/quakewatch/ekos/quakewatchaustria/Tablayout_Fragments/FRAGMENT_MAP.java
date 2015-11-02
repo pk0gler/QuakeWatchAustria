a29f3caf6d73f9bf5bb266b7fd14dac8b5e7dff3
pkogler <pkogler@student.tgm.ac.at>
2015-11-02 13:43:03 +0100

    OSM Update

--- app/src/main/java/com/quakewatch/ekos/quakewatchaustria/Tablayout_Fragments/FRAGMENT_MAP.java
+++ app/src/main/java/com/quakewatch/ekos/quakewatchaustria/Tablayout_Fragments/FRAGMENT_MAP.java
@@ -9,13 +9,19 @@ import android.view.ViewGroup;
 
 import com.quakewatch.ekos.quakewatchaustria.R;
 
+import org.osmdroid.views.MapView;
+
 /**
  * Created by pkogler on 22.10.2015.
  */
 public class FRAGMENT_MAP extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v =inflater.inflate(R.layout.tab_1,container,false);
+        View v =inflater.inflate(R.layout.maplayout,container,false);
+        MapView mapView = (MapView) v.findViewById(R.id.mapview);
+        mapView.setBuiltInZoomControls(true);
+        mapView.setMultiTouchControls(true);
+        mapView.setMaxZoomLevel(10);
         return v;
     }
 }
