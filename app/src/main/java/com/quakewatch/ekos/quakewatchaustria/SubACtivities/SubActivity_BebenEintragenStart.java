f31d13e3327c700c979352674b4ddaf340608ea8
EmreEcevit <eecevit@student.tgm.ac.at>
2015-11-03 21:17:12 +0100

    Zeit + Datum eingabe
    Zeit übernahme wenn "Dieses Beben"

--- app/src/main/java/com/quakewatch/ekos/quakewatchaustria/SubACtivities/SubActivity_BebenEintragenStart.java
+++ app/src/main/java/com/quakewatch/ekos/quakewatchaustria/SubACtivities/SubActivity_BebenEintragenStart.java
@@ -1,56 +1,131 @@
 package com.quakewatch.ekos.quakewatchaustria.SubACtivities;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
-import com.quakewatch.ekos.quakewatchaustria.MainActivity;
 import com.quakewatch.ekos.quakewatchaustria.R;
+import com.quakewatch.ekos.quakewatchaustria.Tablayout_Fragments.Erdbeben;
+
+import java.sql.Time;
+import java.util.Calendar;
 
 /**
  * Created by pkogler on 22.10.2015.
- * Usage:   First screen after pressing "Erdbeben eintragen"
  */
+
 public class SubActivity_BebenEintragenStart extends AppCompatActivity {
     public final static int SUCCESS_RETURN_CODE = 1;
-    private TextView state;
 
-    /**
-     * @param savedInstanceState
-     */
+    private TextView state;
+    private Erdbeben bebenData;
+
+    EditText zeit;
+    EditText datum;
+
+    int hour_x,minute_x;
+    int year_x,month_x,day_x;
+
+    static final int DATE_ID = 0;
+    static final int TIME_ID = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        bebenData = (Erdbeben) getIntent().getExtras().get("bebenData");
         setContentView(R.layout.subactivity_beben);
-        state = (TextView) findViewById(R.id.jetztodned);
+
+        final Calendar cal = Calendar.getInstance();
+        year_x = cal.get(Calendar.YEAR);
+        month_x = cal.get(Calendar.MONTH);
+        day_x = cal.get(Calendar.DAY_OF_MONTH);
+
+        showTimeDialog();
+        showDialogOnClick();
+
         boolean state = (boolean) getIntent().getExtras().get("state");
         if (state) {
-            setUpNow();
+            minute_x = cal.get(Calendar.MINUTE);
+            hour_x = cal.get(Calendar.HOUR);
+            zeit.setText(hour_x+":"+minute_x);
         } else {
-            setUpOther();
+            setUp();
         }
     }
 
-    /**
-     * setUp View for quakes not now
-     */
-    private void setUpOther() {
-        state.setText("Nicht Jetzt");
+    protected Dialog onCreateDialog(int id){
+        if(id == TIME_ID)
+            return new TimePickerDialog(this,kTimePickerListner,hour_x,minute_x,true);
+        if(id == DATE_ID)
+            return new DatePickerDialog(this, dpickerListener,year_x,month_x,day_x);
+        return null;
     }
+    /**
+     * Methoden um die Zeit einstellen zu können
+     */
+    public void showTimeDialog(){
+        zeit = (EditText) findViewById(R.id.editText2);
+        zeit.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View V) {
+                showDialog(TIME_ID);
+            }
+        });
+    }
+    protected TimePickerDialog.OnTimeSetListener kTimePickerListner =
+            new TimePickerDialog.OnTimeSetListener() {
+                @Override
+                public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                    hour_x = hourOfDay;
+                    minute_x = minute;
+                    zeit.setText(hour_x+":"+minute_x);
+                }
+            };
 
     /**
-     * SetUp View for quakes now
+     * Methoden um das Datum eingeben zu können
      */
+    public void showDialogOnClick(){
+        datum = (EditText) findViewById(R.id.editText);
+
+        datum.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View V) {
+                showDialog(DATE_ID);
+            }
+        });
+
+    }
+    private DatePickerDialog.OnDateSetListener dpickerListener
+            = new DatePickerDialog.OnDateSetListener(){
+        public void onDateSet(DatePicker view, int year, int monthOfYear,int dayOfMonth){
+            year_x = year;
+            month_x = monthOfYear+1;
+            day_x = dayOfMonth;
+            datum.setText(year_x+"/"+month_x+"/"+day_x);
+        }
+    };
+
+
+    private void setUp() {
+        //state.setText("Nicht Jetzt\n"+bebenData.getRegion()+"\n"+bebenData.getMag());
+
+    }
+
+
     private void setUpNow() {
-        state.setText("Jetzt");
+        //  state.setText("Jetzt\n"+bebenData.getRegion()+"\n"+bebenData.getMag());
+
     }
 
-    /**
-     * @param item
-     * @return
-     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
--- app/src/main/java/com/quakewatch/ekos/quakewatchaustria/SubACtivities/SubActivity_DiesesBebenEintragen.java
+++ app/src/main/java/com/quakewatch/ekos/quakewatchaustria/SubACtivities/SubActivity_DiesesBebenEintragen.java
@@ -1,43 +1,126 @@
 package com.quakewatch.ekos.quakewatchaustria.SubACtivities;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import com.quakewatch.ekos.quakewatchaustria.R;
 import com.quakewatch.ekos.quakewatchaustria.Tablayout_Fragments.Erdbeben;
 
+import java.util.Calendar;
+
 /**
  * Created by pkogler on 22.10.2015.
  */
+
 public class SubActivity_DiesesBebenEintragen extends AppCompatActivity {
     public final static int SUCCESS_RETURN_CODE = 1;
+
     private TextView state;
     private Erdbeben bebenData;
+
+    EditText zeit;
+    EditText datum;
+
+    int hour_x,minute_x;
+    int year_x,month_x,day_x;
+
+    static final int DATE_ID = 0;
+    static final int TIME_ID = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         bebenData = (Erdbeben) getIntent().getExtras().get("bebenData");
         setContentView(R.layout.subactivity_beben);
-        state = (TextView) findViewById(R.id.jetztodned);
+
+        final Calendar cal = Calendar.getInstance();
+        year_x = cal.get(Calendar.YEAR);
+        month_x = cal.get(Calendar.MONTH);
+        day_x = cal.get(Calendar.DAY_OF_MONTH);
+
+        showTimeDialog();
+        showDialogOnClick();
+
         boolean state = (boolean) getIntent().getExtras().get("state");
         if (state) {
-            setUpNow();
+           setUpNow();
         } else {
             setUp();
         }
    }
 
+    protected Dialog onCreateDialog(int id){
+        if(id == TIME_ID)
+            return new TimePickerDialog(this,kTimePickerListner,hour_x,minute_x,true);
+        if(id == DATE_ID)
+            return new DatePickerDialog(this, dpickerListener,year_x,month_x,day_x);
+        return null;
+    }
+    /**
+     * Methoden um die Zeit einstellen zu können
+     */
+    public void showTimeDialog(){
+        zeit = (EditText) findViewById(R.id.editText2);
+        zeit.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View V) {
+                showDialog(TIME_ID);
+            }
+        });
+    }
+    protected TimePickerDialog.OnTimeSetListener kTimePickerListner =
+            new TimePickerDialog.OnTimeSetListener() {
+                @Override
+                public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                    hour_x = hourOfDay;
+                    minute_x = minute;
+                    zeit.setText(hour_x+":"+minute_x);
+                }
+            };
+
+    /**
+     * Methoden um das Datum eingeben zu können
+     */
+    public void showDialogOnClick(){
+        datum = (EditText) findViewById(R.id.editText);
+
+        datum.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View V) {
+                showDialog(DATE_ID);
+            }
+        });
+
+    }
+    private DatePickerDialog.OnDateSetListener dpickerListener
+            = new DatePickerDialog.OnDateSetListener(){
+        public void onDateSet(DatePicker view, int year, int monthOfYear,int dayOfMonth){
+            year_x = year;
+            month_x = monthOfYear+1;
+            day_x = dayOfMonth;
+            datum.setText(year_x+"/"+month_x+"/"+day_x);
+        }
+    };
+
+
     private void setUp() {
-        state.setText("Nicht Jetzt\n"+bebenData.getRegion()+"\n"+bebenData.getMag());
+        //state.setText("Nicht Jetzt\n"+bebenData.getRegion()+"\n"+bebenData.getMag());
+
     }
 
 
     private void setUpNow() {
-        state.setText("Jetzt\n"+bebenData.getRegion()+"\n"+bebenData.getMag());
+      //  state.setText("Jetzt\n"+bebenData.getRegion()+"\n"+bebenData.getMag());
+
     }
 
     @Override
