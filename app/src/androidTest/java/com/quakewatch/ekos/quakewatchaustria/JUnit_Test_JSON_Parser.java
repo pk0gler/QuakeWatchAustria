c81bc0c4a07bcdf7b48eb69f57a35e9c99798891
pkogler <pkogler@student.tgm.ac.at>
2016-01-14 00:12:47 +0100

    pushed drawables

---
+++ app/src/androidTest/java/com/quakewatch/ekos/quakewatchaustria/JUnit_Test_JSON_Parser.java
@@ -0,0 +1,13 @@
+package com.quakewatch.ekos.quakewatchaustria;
+
+import org.junit.Test;
+
+/**
+ * Created by pkogler on 13.01.2016.
+ */
+public class JUnit_Test_JSON_Parser {
+    @Test
+    public void jsonValidator_ReturnsJson() {
+        assert(true);
+    }
+}
---
+++ app/src/androidTest/java/com/quakewatch/ekos/quakewatchaustria/testJson.java
@@ -0,0 +1,12 @@
+package com.quakewatch.ekos.quakewatchaustria;
+
+import android.test.AndroidTestCase;
+
+/**
+ * Created by pkogler on 14.01.2016.
+ */
+public class testJson extends AndroidTestCase {
+    public void testHTTP() throws Throwable {
+        assert(true);
+    }
+}
