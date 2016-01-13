89980b56e88528c242ba4b744c51a01449ac23db
pkogler <pkogler@student.tgm.ac.at>
2016-01-13 19:55:23 +0100

    changed somth

--- app/src/main/java/com/quakewatch/ekos/quakewatchaustria/SubACtivities/Subactivity_ZusatzFragen.java
+++ app/src/main/java/com/quakewatch/ekos/quakewatchaustria/SubACtivities/Subactivity_ZusatzFragen.java
@@ -39,13 +39,13 @@ public class Subactivity_ZusatzFragen extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // app icon in action bar clicked; go home
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
